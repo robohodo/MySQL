0f347b69a362800d1c6e59670e8e7494810f13e2
Hodo,Robert L <hodo.robert@heb.com>
2018-12-10 16:40:56 -0600

    corrected bad url

--- MySQL5_7_UserData.sh
+++ MySQL5_7_UserData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y install tree yum-utils bind-utils nscd lvm2 lsof at psmisc wget
 
-wget https://github.com/robohodo/MySQL.git
+wget https://github.com/robohodo/MySQL/raw/master/mysql80-community-release-el7-1.noarch.rpm
 yum -y install mysql80-community-release-el7-1.noarch.rpm
 yum-config-manager --disable mysql80-community
 yum-config-manager --enable mysql57-community
